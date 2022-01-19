464416b305044a997a784f94fc5b7c56fb3fa94e
kprzeslawski@student.agh.edu.pl <kprzeslawski@student.agh.edu.pl>
2022-01-19 13:46:58 +0100

    c9.1 test working

--- Gtest/test.cpp
+++ Gtest/test.cpp
@@ -1,14 +1,14 @@
 #include "pch.h"
-#include "MagazynIO/user.h"
+#include "MagazynIO/user.cpp"
 
 TEST(loginWrong, findlogin) {
 	user testuser1 = user("string1","string2","string3",123,321,"string4","string5","string6");
-  EXPECT_EQ(testuser1.findLogin("string1"), 1);
-  EXPECT_TRUE(false);
+  EXPECT_EQ(testuser1.findLogin("string1"), false);
+  EXPECT_TRUE(true);
 }
 
 TEST(loginCorrect, findlogin) {
 	user testuser1 = user("string1", "string2", "string3", 123, 321, "string4", "string5", "string6");
-	EXPECT_EQ(testuser1.findLogin("string5"), 1);
+	EXPECT_EQ(testuser1.findLogin("string5"), true);
 	EXPECT_TRUE(true);
 }
