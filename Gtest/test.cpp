d17ffa1c5c69b6607bf2914fe2a270a4ed91aad2
kprzeslawski@student.agh.edu.pl <kprzeslawski@student.agh.edu.pl>
2022-01-19 13:54:27 +0100

    c10 usertestcomplete

--- Gtest/test.cpp
+++ Gtest/test.cpp
@@ -1,14 +1,28 @@
 #include "pch.h"
 #include "MagazynIO/user.cpp"
 
-TEST(loginWrong, findlogin) {
-	user testuser1 = user("string1","string2","string3",123,321,"string4","string5","string6");
-  EXPECT_EQ(testuser1.findLogin("string1"), false);
-  EXPECT_TRUE(true);
-}
+namespace UserTest {
+	TEST(findlogin, loginWrong) {
+		user testuser1 = user("string1", "string2", "string3", 123, 321, "string4", "string5", "string6");
+		EXPECT_EQ(testuser1.findLogin("string1"), false);
+		EXPECT_TRUE(true);
+	}
 
-TEST(loginCorrect, findlogin) {
-	user testuser1 = user("string1", "string2", "string3", 123, 321, "string4", "string5", "string6");
-	EXPECT_EQ(testuser1.findLogin("string5"), true);
-	EXPECT_TRUE(true);
+	TEST(findlogin, loginCorrect) {
+		user testuser1 = user("string1", "string2", "string3", 123, 321, "string4", "string5", "string6");
+		EXPECT_EQ(testuser1.findLogin("string5"), true);
+		EXPECT_TRUE(true);
+	}
+
+	TEST(checkPassword, passwordCorrect) {
+		user testuser1 = user("string1", "string2", "string3", 123, 321, "string4", "string5", "string6");
+		EXPECT_EQ(testuser1.checkPassword("string6"), true);
+		EXPECT_TRUE(true);
+	}
+
+	TEST(checkPassword, passwordWrong) {
+		user testuser1 = user("string1", "string2", "string3", 123, 321, "string4", "string5", "string6");
+		EXPECT_EQ(testuser1.checkPassword("wrongpassword"), false);
+		EXPECT_TRUE(true);
+	}
 }
